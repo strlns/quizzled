201ac2c8d696416eebeb086245f40e8d64fd50cf
Moritz Rehbach <moritzrehbach@gmail.com>
2021-01-10 20:44:25 +0100

    add material-ui, test styles in registration

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,7 +1,115 @@
-import React from 'react';
+import React, {Component, FormEvent} from 'react';
+import {RouteComponentProps, withRouter} from 'react-router-dom';
 
-export default function SignIn(): JSX.Element {
-    return <div>
-        Nein das darfst du nicht!
-    </div>
+import {SignUpLink} from '../SignUp';
+import Firebase, {withFirebase} from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+import {Box, Button, Container, Grid, TextField} from "@material-ui/core";
+import {PaperBox} from "../MaterialUiTypeScriptHelper/PaperBox";
+
+const SignInPage = () => (
+    <Container maxWidth="lg">
+        <h1>SignIn</h1>
+
+        <Box display="flex" flexDirection="column" alignItems="center">
+            <PaperBox p={4} my={4} elevation={3}>
+                <SignInForm/>
+            </PaperBox>
+            <PaperBox maxWidth="16rem" display="inline-block" p={2} elevation={1} my={4}>
+                <SignUpLink/>
+            </PaperBox>
+        </Box>
+    </Container>
+);
+
+const INITIAL_STATE = {
+    email: '',
+    password: '',
+    error: null
 };
+
+type SignInFormState = {
+    email: string,
+    password: string,
+    error: Error | null
+}
+
+interface SignInFormProps extends RouteComponentProps
+    <any> {
+    firebase: Firebase
+}
+
+class SignInFormBase extends Component<SignInFormProps, SignInFormState> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {...INITIAL_STATE};
+    }
+
+    onSubmit = (event: FormEvent) => {
+        const {email, password} = this.state;
+
+        this.props.firebase
+            .doSignInWithEmailAndPassword(email, password)
+            .then(userCredential => {
+                this.setState({...INITIAL_STATE});
+                this.props.history.push(ROUTES.HOME);
+                console.log(userCredential);
+            })
+            .catch(error => {
+                this.setState({error});
+            });
+
+        event.preventDefault();
+    };
+
+    render() {
+        const minLengthInputProps = {
+            minLength: 8
+        };
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <Grid container spacing={4} justify="center">
+                    <Grid item xs={12} lg={6}>
+                        <TextField
+                            name="email"
+                            value={this.state.email}
+                            onChange={e => this.setState({email: e.target.value})}
+                            type="text"
+                            required={true}
+                            label="E-Mail-Adresse"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <TextField
+                            name="password"
+                            value={this.state.password}
+                            onChange={e => this.setState({password: e.target.value})}
+                            type="password"
+                            inputProps={minLengthInputProps}
+                            required={true}
+                            label="Password"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button variant="contained" color="primary" size="large"
+                                type="submit">
+                            Sign In
+                        </Button>
+                    </Grid>
+
+                    {this.state.error && <Grid item xs={12}><p>{this.state.error.message}</p></Grid>}
+                </Grid>
+            </form>
+        );
+    }
+}
+
+const SignInForm = withRouter(withFirebase(SignInFormBase));
+
+export default SignInPage;
+
+export {SignInForm};
--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,17 +1,18 @@
 import React, {Component, FormEvent} from 'react';
-import {Link, withRouter, RouteComponentProps} from 'react-router-dom';
+import {withRouter, RouteComponentProps, Link} from 'react-router-dom';
 
 import * as ROUTES from '../../constants/routes';
 
-import {withFirebase} from "../Firebase/context";
+import {withFirebase} from "../Firebase";
 import Firebase from "../Firebase";
+import {Box, Button, Container, Grid, Paper, TextField} from "@material-ui/core";
 
 type SignUpFormState = {
     username: string,
     email: string,
     password: string,
     passwordRepeat: string,
-    error: Error|null,
+    error: Error | null,
 }
 
 interface SignUpFormProps extends RouteComponentProps<any> {
@@ -28,24 +29,23 @@ const INITIAL_STATE = {
 
 class SignUpFormBase extends Component<SignUpFormProps, SignUpFormState> {
 
-    constructor(props: any) {
+    constructor(props: SignUpFormProps) {
         super(props);
 
-        this.state = { ...INITIAL_STATE }; //shallow clone
+        this.state = {...INITIAL_STATE}; //shallow clone
     }
 
     onSubmit = (event: FormEvent) => {
-        const { email, password } = this.state;
+        const {email, password} = this.state;
 
         this.props.firebase
             .doCreateUserWithEmailAndPassword(email, password)
             .then(authUser => {
-                this.setState({ ...INITIAL_STATE });
-                console.log(authUser);
+                this.setState({...INITIAL_STATE});
                 this.props.history.push(ROUTES.HOME);
             })
             .catch(error => {
-                this.setState({ error });
+                this.setState({error});
             });
 
         event.preventDefault();
@@ -59,68 +59,96 @@ class SignUpFormBase extends Component<SignUpFormProps, SignUpFormState> {
     }
 
     render() {
+        const minLengthInputProps = {
+            minLength: 8
+        };
         return (
-            <form onSubmit={this.onSubmit}>
-                <input
-                    name="username"
-                    value={this.state.username}
-                    onChange={e => this.setState(() => ({username: e.target.value}))}
-                    type="text"
-                    required={true}
-                    placeholder="Full Name"
-                />
-                <input
-                    name="email"
-                    value={this.state.email}
-                    onChange={e => this.setState(() => ({email: e.target.value}))}
-                    type="email"
-                    required={true}
-                    placeholder="Email Address"
-                />
-                <input
-                    name="password"
-                    value={this.state.password}
-                    onChange={e => this.setState(() => ({password: e.target.value}))}
-                    type="password"
-                    minLength={8}
-                    required={true}
-                    placeholder="Password"
-                />
-                <input
-                    name="passwordRepeat"
-                    value={this.state.passwordRepeat}
-                    onChange={e => this.setState(() => ({passwordRepeat: e.target.value}))}
-                    type="password"
-                    minLength={8}
-                    required={true}
-                    placeholder="Confirm Password"
-                />
-                <button disabled={this.isInvalid()}
-                        type="submit">
-                    Sign Up
-                </button>
+            <Container maxWidth="lg">
+                <Box p={4} component={Paper}>
+                    <form onSubmit={this.onSubmit}>
+                        <Grid container spacing={4} justify="center">
+                            <Grid item xs={12} lg={6}>
+                                <TextField
+                                    name="username"
+                                    value={this.state.username}
+                                    onChange={e => this.setState({username: e.target.value})}
+                                    type="text"
+                                    required={true}
+                                    label="Full Name"
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item xs={12} lg={6}>
+                                <TextField
+                                    name="email"
+                                    value={this.state.email}
+                                    onChange={e => this.setState({email: e.target.value})}
+                                    type="email"
+                                    required={true}
+                                    label="Email Address"
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item xs={12} lg={6}>
+                                <TextField
+                                    name="password"
+                                    value={this.state.password}
+                                    onChange={e => this.setState({password: e.target.value})}
+                                    type="password"
+                                    inputProps={minLengthInputProps}
+                                    required={true}
+                                    label="Password"
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item xs={12} lg={6}>
+                                <TextField
+                                    name="passwordRepeat"
+                                    value={this.state.passwordRepeat}
+                                    onChange={e => this.setState({passwordRepeat: e.target.value})}
+                                    type="password"
+                                    inputProps={minLengthInputProps}
+                                    required={true}
+                                    label="Confirm Password"
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item>
+                                <Button variant="contained" color="primary" size="large" disabled={this.isInvalid()}
+                                        type="submit">
+                                    Sign Up
+                                </Button>
+                            </Grid>
 
-                {this.state.error && <p>{this.state.error.message}</p>}
-            </form>
+                            {this.state.error && <Grid item xs={12}><p>{this.state.error.message}</p></Grid>}
+                        </Grid>
+                    </form>
+                </Box>
+            </Container>
         );
     }
 }
 
-const SignUpForm = withRouter(withFirebase(SignUpFormBase));
-
 const SignUpLink = () => (
-    <p>
-        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-    </p>
+    <div>
+        <p>
+            Noch kein Account vorhanden?
+        </p>
+        <p>
+            <Link to={ROUTES.SIGN_UP}>Hier</Link> Account erstellen
+        </p>
+    </div>
 );
+
+const SignUpForm = withRouter(withFirebase(SignUpFormBase));
 
 const SignUpPage = () => (
     <div>
         <h1>SignUp</h1>
-        <SignUpForm />
+        <SignUpForm/>
     </div>
 );
 
 export default SignUpPage;
 
-export { SignUpForm, SignUpLink };
+export {SignUpForm, SignUpLink};
